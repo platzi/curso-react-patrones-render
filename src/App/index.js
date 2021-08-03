f807c09f7fc2419935620afac4069e221b1259da
Juan David Castro <me@juandc.co>
2021-08-03 14:02:18 -0500

    Clase 04 en orden de grabación

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,54 @@
 import React from 'react';
-import { TodoProvider } from '../TodoContext';
-import { AppUI } from './AppUI';
-
+// import { TodoProvider } from '../TodoContext';
+// import { TodoCounter } from '../TodoCounter';
+// import { AppUI } from './AppUI';
 
 function App() {
+  const [state, setState] = React.useState('estado compartido');
+  
   return (
-    <TodoProvider>
-      <AppUI />
-    </TodoProvider>
+    <React.Fragment>
+      <TodoHeader>
+        <TodoCounter />
+        <TodoSearch />
+      </TodoHeader>
+
+      <TodoList>
+        <TodoItem state={state} />
+      </TodoList>
+    </React.Fragment>
   );
 }
+function TodoHeader({ children }) {
+  return (
+    <header>
+      {children}
+    </header>
+  );
+}
+function TodoList({ children }) {
+  return (
+    <section className="TodoList-container">
+      {children}
+    </section>
+  );
+}
+function TodoCounter() {
+  return <p>TodoCounter</p>;
+}
+function TodoSearch() {
+  return <p>TodoSearch</p>;
+}
+function TodoItem({ state }) {
+  return <p>TodoItem: {state}</p>;
+}
+
+// function App() {
+//   return (
+//     <TodoProvider>
+//       <AppUI />
+//     </TodoProvider>
+//   );
+// }
 
 export default App;
