deece168e65a669201cea0ad511d91764cff1e72
Juan David Castro <me@juandc.co>
2021-08-04 12:39:08 -0500

    Clase 13 en orden de grabación

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App/index.js';
+// import App from './App/index.js';
 import './index.css';
 
+function App(props) {
+  return (
+    <h1>¡{props.saludo}, {props.nombre}!</h1>
+  );
+}
+
+function withSaludo(saludo) {
+  return function WrappedComponentWithSaludo(WrappedComponent) {
+    return function ComponenteDeVerdad(props) {
+      return (
+        <React.Fragment>
+          <WrappedComponent {...props} saludo={saludo} />
+          <p>Estamos acompañando al WrappedComponent</p>
+        </React.Fragment>
+      );
+    }
+  }
+}
+
+const AppWithSaludo = withSaludo('Wenas')(App);
+
 ReactDOM.render(
-  <App />,
+  <AppWithSaludo nombre="Juanita" />,
+  // <App saludo="Buenas" nombre="Nath" />,
   document.getElementById('root')
 );
